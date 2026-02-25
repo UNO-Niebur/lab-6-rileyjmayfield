e5cd8177f99d49a9e7fa14a53db0b6b429ed7c61
rileyjmayfield <rileyjmayfield@gmail.com>
2026-02-25 04:16:31 +0000

    WordIndex.py Done

--- WordIndex.py
+++ WordIndex.py
@@ -4,16 +4,32 @@
 #Assignment:
 
 def main():
-  textFile = open("fish.txt", 'r')
-  
+  textFile = open("gettysberg.txt", 'r')
+  lineNumber = 0
   words = {} #create an empty dictionary
   
+  for line in textFile:
+    lineNumber = lineNumber + 1
+    wordlist = line.split()
+   
+    for w in wordlist:
+      w = w.lower()
+      w = w.replace("," ,"")
+      w = w.replace("." ,"")
+      w = w.replace("?" ,"")
+      w = w.replace("!" ,"")
+      w = w.replace("." ,"")
+      w = w.replace("\"" ,"")
+      
+      
+      if w in words:
+        if lineNumber not in words[w]:
+         words[w].append(lineNumber)
+      else:
+        words[w] = [lineNumber]
   
-  print ("fish" in words) #is a word already in the dictionary?
-  words["fish"] = [2]     #add a list to the dictionary
-  print ("fish" in words) #is the word there now?
-  words["fish"].append(5) #add to an existing list
-  print(words)
+  for word in words:
+    print(word, words[word])
 
 
 if __name__ == '__main__':
